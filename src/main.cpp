598d5bc6db2fff2b243bea4bafbc17cecdf582cd
Christophe Cossou <ccossou@gmail.com>
2020-10-04 22:56:01 +0200

    Try to make the screen working (not working for now, unsure why)

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,17 @@
 #include <Arduino.h>
+#include <LiquidCrystal_I2C.h>
+
+LiquidCrystal_I2C lcd(0x27,16,2);  // set the LCD address to 0x27 for a 16 chars and 1 line display
 
 void setup() {
-
-    pinMode(13,OUTPUT); Serial.begin(9600); }
+    lcd.init();                      // initialize the lcd
+    // Print a message to the LCD.
+    lcd.backlight();
+}
 
 void loop() {
-
-    Serial.println("Hi");
-    digitalWrite(13,HIGH);
+    lcd.clear();
+    lcd.setCursor(0,0); // positionne le curseur à la colonne 1 et à la ligne 2
+    lcd.print("Hello World!");
     delay(1000);
-    digitalWrite(13,LOW); delay(1000);
 }
