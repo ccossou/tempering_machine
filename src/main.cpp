0a1918a4962bc6fb048b135cf28608d00d7bd81a
Christophe Cossou <ccossou@gmail.com>
2020-10-13 22:54:43 +0200

    I can retrieve temperature from sensor.
    TODO: Find how to print it and limit significative digits.

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,12 @@
 #include <Arduino.h>
 #include <LiquidCrystal_I2C.h>
+#include <OneWire.h>
+#include <DallasTemperature.h>
+
+#define ONE_WIRE_BUS 4
+
+OneWire oneWire(ONE_WIRE_BUS);
+DallasTemperature temp_sensor(&oneWire);
 
 LiquidCrystal_I2C lcd(0x27,16,2);  // set the LCD address to 0x27 for a 16 chars and 1 line display
 
@@ -7,13 +14,21 @@ void setup() {
     lcd.init();                      // initialize the lcd
     // Print a message to the LCD.
     lcd.backlight();
+
+    temp_sensor.begin();
+
     Serial.begin(9600);
 }
 
 void loop() {
+    temp_sensor.requestTemperatures(); // Send the command to get temperatures
+
     lcd.clear();
     lcd.setCursor(0,0); // positionne le curseur à la colonne 1 et à la ligne 2
+    float current_temperature{temp_sensor.getTempCByIndex(0)};
+
     lcd.print("T: 25.2/28.0" "\xDF" "C");
     Serial.println("Hello World!");
+    Serial.println(current_temperature);
     delay(1000);
 }
