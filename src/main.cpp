fb2c8b89a44ea454ddc76843bb77aee7013141a8
Christophe Cossou <ccossou@gmail.com>
2020-10-16 22:45:55 +0200

    Create function in charge of display to LCD and serial

--- src/main.cpp
+++ src/main.cpp
@@ -26,29 +26,40 @@ void setup() {
 
 }
 
-void loop() {
-    temp_sensor.requestTemperatures(); // Send the command to get temperatures
-
-    lcd.clear();
-
-    float current_temperature{temp_sensor.getTempCByIndex(0)};
-    dtostrf(current_temperature, 4, 1, current_temp_str);
-    dtostrf(requested_temperature, 4, 1, requested_temp_str);
+/** Update temperature display, both on lcd and serial
+ *
+ * @param current_t Current temperature measured by the sensor
+ * @param requested_t Requested temperature by the user
+ */
+void update_display(float current_t, float requested_t) {
+    dtostrf(current_t, 4, 1, current_temp_str);
+    dtostrf(requested_t, 4, 1, requested_temp_str);
 
     lcd.setCursor(0,0); // positionne le curseur at (col, row)
-    lcd.print("T: 25.2/28.0" "\xDF" "C");
-
-    // Print temp to serial monitor
-    Serial.print("Current temperature: ");
-    Serial.print(current_temp_str);
-    Serial.print(char(176));
-    Serial.println("C");
+    lcd.print("T: 25.2/28.0" "\xDF" "C");  // Temperatures here are fake because overwritten by the real one later on
 
     lcd.setCursor(3, 0);
     lcd.print(current_temp_str);
 
     lcd.setCursor(8, 0);
     lcd.print(requested_temp_str);
+
+    // Print temp to serial monitor
+    Serial.print("Current temperature: ");
+    Serial.print(current_temp_str);
+    Serial.print(char(176));
+    Serial.println("C");
+}
+
+void loop() {
+    temp_sensor.requestTemperatures(); // Send the command to get temperatures
+
+    lcd.clear();
+
+    float current_temperature{temp_sensor.getTempCByIndex(0)};
+
+    update_display(current_temperature, requested_temperature);
+
 
     delay(1000);
 }
